e4bac3fc46f305156b97db467195be9a553290cc
Casper Beyer <caspervonb@qup.io>
2015-02-06 20:58:06 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+#!/usr/bin/env node
+
+var http = require('http');
+var fs = require('fs');
+var child = require('child_process');
+var util = require('util');
+
+function file(req, res) {
+  var filepath = req.url;
+  fs.exists(filepath, function(exists) {
+    if (exists) {
+      fs.readFile(filepath, function(error, buffer) {
+        if (err) {
+          res.writeHead(500);
+          res.write('500');
+          res.end();
+        }
+      });
+    } else {
+      res.writeHead(404);
+      res.write('404');
+      res.end();
+    }
+  });
+}
+
+function bundle(req, res) {
+  res.setHeader('content-type', 'text/javascript');
+
+  var argv = process.argv.slice(2);
+  var cmd = util.format('browserify', argv.join(' '));
+  var bundler = child.exec(cmd, function(error, stdout, stderr) {
+    if (error) {
+      res.write(error.toString());
+    }
+
+    res.end(stdout);
+  });
+}
+
+function index(req, res) {
+  fs.exists('index.html', function(exists) {
+    if (exists) {
+      return file(req, res);
+    } else {
+      res.setHeader('content-type', 'text/html');
+      res.end('<!doctype html><head><meta charset="utf-8"></head><body><script src="index.js"></script></body></html>');
+    }
+  });
+}
+
+function watch(req, res) {
+	req.setTimeout(Infinity);
+
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
+  res.write('\n');
+  var listener = function(event, filename) {
+    // Skip dot files
+    if (filename[0] == '.') {
+      return;
+    }
+
+    // Assume the bundle has changed when any javascript file changes.
+    if (filename.indexOf('.js') > 0) {
+      filename = 'index.js';
+    }
+
+    res.write('data: ' + filename + '\n\n');
+  };
+
+  watcher.on('change', listener);
+  res.on('close', function() {
+    watcher.removeListener('change', listener);
+  });
+}
+
+var watcher = fs.watch('.', {
+  recursive: true,
+});
+
+watcher.on('change', function(event, filename) {
+  console.log(event, filename);
+});
+
+var server = http.createServer();
+server.on('request', function(req, res) {
+  switch (req.url) {
+    case '/watch':
+      return watch(req, res);
+
+    case '/index.js':
+      return bundle(req, res);
+
+    case '/':
+      return index(req, res);
+
+    default:
+      return file(req, res);
+  }
+});
+
+server.listen(3000, function () {
+  var address = server.address();
+  console.log('serving on port %d', address.port);
+});
